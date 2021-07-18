da3ca67d09107314cbbb24352a895e4538b794c5
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-07-18 22:14:07 +0900

    [Prac] 스파르타 week01

---
+++ core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,24 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP는 10% 할인이 적용되어야 한다.")
+    void vip_o(){
+        // given
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+        // then
+        discountPolicy.discount(member, )
+        //
+    }
+
+}
