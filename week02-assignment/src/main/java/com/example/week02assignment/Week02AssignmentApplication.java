e85797e11fea6b4399189441ff75f7111ce9ddf2
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-07-19 22:47:16 +0900

    [Prac] week02 assignment project setting

---
+++ week02-assignment/src/main/java/com/example/week02assignment/Week02AssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.week02assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week02AssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week02AssignmentApplication.class, args);
+	}
+
+}
---
+++ week02-assignment/src/test/java/com/example/week02assignment/Week02AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.week02assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week02AssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
