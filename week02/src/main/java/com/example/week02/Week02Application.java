2f8f49e5c423922b84b913ccdb98c49466f10313
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-07-19 20:49:27 +0900

    [Prac] week02 JPA update, delete 연습

--- week02/src/main/java/com/example/week02/Week02Application.java
+++ week02/src/main/java/com/example/week02/Week02Application.java
@@ -2,6 +2,7 @@ package com.example.week02;
 
 import com.example.week02.domain.Course;
 import com.example.week02.domain.CourseRepository;
+import com.example.week02.service.CourseService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,25 +15,47 @@ import java.util.List;
 @SpringBootApplication
 public class Week02Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Week02Application.class, args);
-	}
-	// Week02Application.java 의 main 함수 아래에 붙여주세요.
-	@Bean
-	public CommandLineRunner demo(CourseRepository repository) {
-		return (args) -> {
+    public static void main(String[] args) {
+        SpringApplication.run(Week02Application.class, args);
+    }
 
-			Course course1 = new Course("웹 개발의 봄 Spring", "남병관");
-			repository.save(course1);
+    // Week02Application.java 의 main 함수 아래에 붙여주세요.
+    @Bean
+    public CommandLineRunner demo(CourseRepository repository, CourseService courseService) {
+        return (args) -> {
 
-			// select * from course
-			List<Course> courseList = repository.findAll();
+            // 데이터 저장하기
+            repository.save(new Course("프론트엔드의 꽃, 리액트", "임민영"));
 
-			for(int i=0; i<courseList.size(); i++){
-				Course c = courseList.get(i);
-				System.out.println(c.getTitle());
-				System.out.println(c.getTutor());
-			}
-		};
-	}
+            // 데이터 전부 조회하기 -> findAll
+            List<Course> courseList = repository.findAll();
+            for (int i = 0; i < courseList.size(); i++) {
+                Course course = courseList.get(i);
+                System.out.println(course.getId());
+                System.out.println(course.getTitle());
+                System.out.println(course.getTutor());
+            }
+
+            // 기존의 데이터 변 - update
+            Course new_course = new Course("웹개발의 봄 Spring", "임민영");
+            courseService.update(1L, new_course);
+            courseList = repository.findAll();
+            for (int i = 0; i < courseList.size(); i++) {
+                Course course = courseList.get(i);
+                System.out.println(course.getId());
+                System.out.println(course.getTitle());
+                System.out.println(course.getTutor());
+            }
+
+            // 데이터 하나 조회하기 -> findById
+            Course course = repository.findById(1L).orElseThrow(
+                    // 가리키는게 없을 때의 예외
+                    () -> new IllegalArgumentException("해당 아이디가 존재하지 않습니다.")
+            );
+
+            // delete
+            repository.deleteAll();
+
+        };
+    }
 }
---
+++ week02/src/main/java/com/example/week02/service/CourseService.java
@@ -0,0 +1,31 @@
+package com.example.week02.service;
+
+import com.example.week02.domain.Course;
+import com.example.week02.domain.CourseRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service // 스프링에게 이 클래스는 서비스임을 명시함
+public class CourseService {
+
+    // final : 서비스에게 꼭 필요한 녀석임을 명시함.
+    private final CourseRepository courseRepository;
+
+    // 생성자를 통해 Service 클래스를 만들때 courseRepositoy를 넣어주도록 함
+    // 스프링에게 알려줌
+    public CourseService(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+    // SQL 쿼리가 일어나야 함을 알려줌.
+    // id: 업데이트할 것이 무엇인지
+    // course: 업데이트할 내용이 무엇인ㅈ
+    @Transactional
+    public Long update(Long id, Course course){
+        Course course1 = courseRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 아이디가 존재하지 않습니다.")
+        );
+        course1.update(course); // course 클래스 안의 메서드 update 사
+        return course1.getId();
+    }
+}
