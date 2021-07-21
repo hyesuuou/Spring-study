9acbc6d75e0d05502e70ac3d55d462b90247df63
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-07-21 19:46:18 +0900

    [Prac] Project initial setting

---
+++ SpringPrac/src/main/java/com/example/SpringPrac/SpringPracApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringPrac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPracApplication.class, args);
+	}
+
+}
---
+++ SpringPrac/src/test/java/com/example/SpringPrac/SpringPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringPrac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
